94762ca3d4052bb96ebb708f6f2ea2efe0ef7843
Administrator <siddhiseth3009@gmail.com>
2020-10-06 01:59:47 +0530

    [Siddhi]Refactor computer movement method removed

--- TicTacToeGame/TicTacToeGame.cs
+++ TicTacToeGame/TicTacToeGame.cs
@@ -80,21 +80,6 @@ namespace TicTacToeGame
                 PlayerMovement(choice);
             }
         }
-        public void ComputerMovement(char compChoice)
-        {
-            Random random = new Random();
-            int computerChoice = random.Next(1, 10);
-            bool emptyPosition = PositionCheck(computerChoice);
-            if (emptyPosition == true)
-            {
-                board[computerChoice] = compChoice;
-                ShowBoard();
-            }
-            else
-            {
-                ComputerMovement(compChoice);
-            }
-        }
         public Player Toss()
         {
             Random random = new Random();
@@ -108,103 +93,6 @@ namespace TicTacToeGame
             {
                 Console.WriteLine("Computer will begin");
                 return Player.COMPUTER;
-            }
-        }
-        public void Play(char userChoice, char compChoice)
-        {
-            Player player = Toss();
-            int win = 0;
-            while (win == 0)
-            {
-                if (player.Equals(Player.USER))
-                {
-                    PlayerMovement(userChoice);
-                    player = Player.COMPUTER;
-                }
-                else
-                {
-                    ComputerMovement(compChoice);
-                    player = Player.USER;
-                }
-                win = CheckWin();
-                if (win == 1)
-                {
-                    if (player == Player.USER)
-                    {
-                        Console.WriteLine("Computer won");
-                        break;
-                    }
-                    if (player == Player.COMPUTER)
-                    {
-                        Console.WriteLine("User won");
-                        break;
-                    }
-                }
-                else if (win == -1)
-                {
-                    Console.WriteLine("Game draw");
-                    break;
-                }
-                else
-                    continue;
-            }
-        }
-        public int CheckWin()
-        {
-            #region Horzontal Winning Condtion
-            //Winning Condition For First Row   
-            if (board[1] == board[2] && board[2] == board[3] && board[3] != ' ')
-            {
-                return 1;
-            }
-            //Winning Condition For Second Row   
-            else if (board[4] == board[5] && board[5] == board[6] && board[6] != ' ')
-            {
-                return 1;
-            }
-            //Winning Condition For Third Row   
-            else if (board[6] == board[7] && board[7] == board[8] && board[8] != ' ')
-            {
-                return 1;
-            }
-            #endregion
-            #region vertical Winning Condtion
-            //Winning Condition For First Column       
-            else if (board[1] == board[4] && board[4] == board[7] && board[7] != ' ')
-            {
-                return 1;
-            }
-            //Winning Condition For Second Column  
-            else if (board[2] == board[5] && board[5] == board[8] && board[8] != ' ')
-            {
-                return 1;
-            }
-            //Winning Condition For Third Column  
-            else if (board[3] == board[6] && board[6] == board[9] && board[9] != ' ')
-            {
-                return 1;
-            }
-            #endregion
-            #region Diagonal Winning Condition
-            else if (board[1] == board[5] && board[5] == board[9] && board[9] != ' ')
-            {
-                return 1;
-            }
-            else if (board[3] == board[5] && board[5] == board[7] && board[7] != ' ')
-            {
-                return 1;
-            }
-            #endregion
-            #region Checking For Draw
-            // If all the cells or values filled with X or O then any player has won the match  
-            else if (board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' ' && board[5] != ' ' && board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ')
-            {
-                return -1;
-            }
-            #endregion
-            else
-            {
-                return 0;
             }
         }
     }
