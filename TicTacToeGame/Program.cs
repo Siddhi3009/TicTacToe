902358535ce4dcf2b88a1211de902d1c50ca6141
Administrator <siddhiseth3009@gmail.com>
2020-10-05 15:00:57 +0530

    [Siddhi]Add Choice method added

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -6,8 +6,11 @@ namespace TicTacToeGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
             TicTacToeGame game = new TicTacToeGame();
             game.CreateBoard();
+            string computerChoice = "Computer:" + game.Choice();
+            string userChoice = "User:" + game.Choice();
         }
     }
 }
