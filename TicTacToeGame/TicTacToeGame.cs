b83b5300c31b7b95c4f7f21ef224a0f45217fd6b
Administrator <siddhiseth3009@gmail.com>
2020-10-05 14:21:20 +0530

    [Siddhi]Add CreateBoard Method added

--- TicTacToeGame/TicTacToeGame.cs
+++ TicTacToeGame/TicTacToeGame.cs
@@ -6,5 +6,18 @@ namespace TicTacToeGame
 {
     class TicTacToeGame
     {
+        public char[] board;
+        public TicTacToeGame()
+        {
+            board = new char[10];
+        }
+        public void CreateBoard()
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            for (int position = 1; position < 10; position++)
+            {
+                game.board[position] = ' ';
+            }
+        }
     }
 }
