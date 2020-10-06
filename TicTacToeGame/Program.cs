c9eef155f236bad065b6b43839e38a87ebd432bd
Administrator <siddhiseth3009@gmail.com>
2020-10-06 15:24:18 +0530

    [Siddhi]Refactor Program refactored to play another game if interested

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -8,15 +8,21 @@ namespace TicTacToeGame
         {
             Console.WriteLine("Welcome to Tic Tac Toe Game");
             TicTacToeGame game = new TicTacToeGame();
-            game.CreateBoard();
-            char userChoice = game.Choice();
-            char compChoice;
-            if (userChoice == 'X')
-                compChoice = '0';
-            else
-                compChoice = 'X';
-            game.ShowBoard();
-            game.Play(userChoice, compChoice);
+            int playAnotherGame = 1;
+            while (playAnotherGame == 1)
+            {
+                game.CreateBoard();
+                char userChoice = game.Choice();
+                char compChoice;
+                if (userChoice == 'X')
+                    compChoice = '0';
+                else
+                    compChoice = 'X';
+                game.ShowBoard();
+                game.Play(userChoice, compChoice);
+                Console.WriteLine("Do you want to play another game? \n1. Yes\n2. No");
+                playAnotherGame = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
